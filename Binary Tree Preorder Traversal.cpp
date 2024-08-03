802b098cee8647bcfe11f229a85fb9eea69c8566
SUSHANTH R HEGDE <120232856+SushanthRHegde@users.noreply.github.com>
2024-08-03 21:54:17 +0530

    Update Binary Tree Preorder Traversal.cpp

--- Binary Tree Preorder Traversal.cpp
+++ Binary Tree Preorder Traversal.cpp
@@ -26,3 +26,24 @@ public:
         return ans;
     }   
 };
+
+
+// *****************************     Iterative Method   ***************************************************************************************
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL )return ans;
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            ans.push_back(root->val);
+            if(root->right != NULL)st.push(root->right);
+            if(root->left != NULL) st.push(root->left);
+        }
+        return ans;
+    }
+};
