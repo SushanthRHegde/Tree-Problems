c797b03a069466e5cc04f1a39634e66d88d06ffb
SUSHANTH R HEGDE <120232856+SushanthRHegde@users.noreply.github.com>
2024-08-04 00:35:25 +0530

    Create Symmetric Tree_Mirror.cpp

---
+++ Symmetric Tree_Mirror.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool solve(TreeNode* left, TreeNode* right){
+        if(left == NULL && right == NULL) return true;
+        if(left == NULL || right == NULL) return false;
+        return (left->val == right->val && solve(left->left, right->right) && solve(left->right, right->left));
+    }
+    bool isSymmetric(TreeNode* root) {
+        return solve(root->left, root->right);
+    }
+};
